2703fe2cd53d766d361ac55f4bc97797cefe970b
kotarf <happydemon912@gmail.com>
2013-09-20 22:54:46 -0400

    Unit test to sort a bookmark

--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,5 @@
 QUnit.begin(function() {
-	SmartBookmarkSorter.setApiKey("f3c34a16c40928e82a9b1d6d8997e78464da139a");
+	SmartBookmarkSorter.setApiKey("");
 });
 
 test( "test", function() {
@@ -36,10 +36,21 @@ asyncTest("Title request is successfully made", 1, function() {
 	SmartBookmarkSorter.alchemyTitle("http://stackoverflow.com", callback);
 });
 
-/*
-asyncTest("A single bookmark is sorted", 1, function() {
+
+asyncTest("A single bookmark is sorted", 2, function() {
 	var callback = function() {
-		
+
+		chrome.bookmarks.search("Stack Overflow Test", function (results) {
+			var result = results[0],
+				parentId = result.parentId,
+				id = result.id;
+				
+			chrome.bookmarks.remove(id, $.noop)
+			
+			notEqual(parentId, 1, "parent is not other bookmarks");
+			ok(parentId, "parent exists");
+			start();
+		});
 	};
 	
 	var bookmark = {
@@ -47,11 +58,12 @@ asyncTest("A single bookmark is sorted", 1, function() {
 		url: "http://stackoverflow.com"
 	}
 	var deferred = $.Deferred();
+	
 	chrome.bookmarks.create(bookmark, function(result) {
-		sortBookmark(result, deferred,
+		SmartBookmarkSorter.sortBookmark(result, callback, deferred)
 	});
 });
-* */
+
 
 QUnit.done(function() {
 	var items = $.totalStorage.getAll();
