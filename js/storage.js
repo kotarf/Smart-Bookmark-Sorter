cc18d920ca6c57b551bb58745af4edbf049547d9
Frank Kotarski <kotarf@alum.rpi.edu>
2015-08-14 17:19:55 -0400

    Refactored storage module to generate getters/setters

--- js/storage.js
+++ js/storage.js
@@ -1,264 +1,50 @@
 /**
- * Created by kotarf on 8/25/14.
+ * Created by kotarf on 8/14/2015.
  */
-define(['jqueryhelpers', 'config'], function(jhelpers, config) {
-    return {
-        /**
-         * Sets the api key in local storage
-         * @param {string} apikey The apikey to set
-         * @config {string} [apiStorageKey] Local storage key for api key
-         */
-        setApiKey: function (apikey) {
-            jhelpers.jQueryStorageSetValue(config.apiStorageKey, apikey);
-        },
+define(['underscore', 'jqueryhelpers', 'config'], function(_, jhelpers, config) {
+    // Add new local storage variables here
+    var tokens = [
+        {name: 'ApiKey'},
+        {name: 'AutoOnCreate'},
+        {name: 'AutoInterval'},
+        {name: 'AutoPrioritize'},
+        {name: 'AutoOn'},
+        {name: 'OldBookmarkDays', def: config.oldBookmarkDaysDefault},
+        {name: 'IsOnManualSorting'},
+        {name: 'IsOnCreateSorting'},
+        {name: 'IsOnAlarmSorting'},
+        {name: 'MaxTaxonomyLevels', def: config.defaultTaxonomyLevels},
+        {name: 'IsOnCullBookmarks'},
+        {name: 'CullNumber', def: config.defaultCullNumber},
+        {name: 'SortAction'},
+        {name: 'ArchivesName', def: config.archivesFolder},
+        {name: 'AutosortCounter'},
+    ];
 
-        /**
-         * Gets the api key in local storage
-         * @config {string} [apiStorageKey] Local storage key for api key
-         * @returns {string}
-         */
-        getApiKey: function () {
-            return jhelpers.jQueryStorageGetValue(config.apiStorageKey);
-        },
+    var getter = function(key, def) {
+        return jhelpers.jQueryStorageGetValue(key) || def;
+    }
 
-        /**
-         * Sets auto create on
-         * @param {bool} value The boolean to set
-         * @config {string} [autoSortCreateKey] Local storage key auto create on
-         */
-        setAutoOnCreate: function (value) {
-            jhelpers.jQueryStorageSetValue(config.autoSortCreateKey, value);
-        },
+    var setter = function(key, value) {
+        jhelpers.jQueryStorageSetValue(key, value);
+    }
 
-        /**
-         * Gets the api key in local storage
-         * @config {string} [autoSortCreateKey] Local storage key for api key
-         * @returns {bool}
-         */
-        getAutoOnCreate: function () {
-            return jhelpers.jQueryStorageGetValue(config.autoSortCreateKey);
-        },
+    var module = {};
 
-        /**
-         * Sets the auto interval  in local storage
-         * @config {string} [autoSortTimedKey] Local storage key for timed sort
-         * @param {bool} value The boolean to set
-         */
-        setAutoInterval: function (value) {
-            jhelpers.jQueryStorageSetValue(config.autoSortTimedKey, value);
-        },
+    _.each(tokens, function(element) {
+        var name = element.name,
+            def = element.def;
 
-        /**
-         * Get the auto interval in local storage
-         * @config {string} [autoSortTimedKey] Local storage key for timed sort
-         * @returns {bool}
-         */
-        getAutoInterval: function () {
-            return jhelpers.jQueryStorageGetValue(config.autoSortTimedKey);
-        },
+        // Generate getter
+        var getfunct = _.partial(getter, name, def);
 
-        /**
-         * Sets the auto prioritize  in local storage
-         * @config {string} [autoSortPriorityKey] Local storage key for priority sort
-         * @param {bool} value The boolean to set
-         */
-        setAutoPrioritize: function (value) {
-            jhelpers.jQueryStorageSetValue(config.autoSortPriorityKey, value);
-        },
+        // Generate setter
+        var setfunct = _.partial(setter, name);
 
-        /**
-         * Get the auto interval in local storage
-         * @config {string} [autoSortPriorityKey] Local storage key for priority sort
-         * @returns {bool}
-         */
-        getAutoPrioritize: function () {
-            return jhelpers.jQueryStorageGetValue(config.autoSortPriorityKey);
-        },
+        // Set getter and setter on module
+        module['get' + name] = getfunct;
+        module['set' + name] = setfunct;
+    });
 
-        /**
-         * Sets the auto sort in local storage
-         * @config {string} [autoSortActiveKey] Local storage key for auto sort on
-         * @param {bool} value The boolean to set
-         */
-        setAutoOn: function (value) {
-            jhelpers.jQueryStorageSetValue(config.autoSortActiveKey, value);
-        },
-
-        /**
-         * Gets the auto sort in local storage
-         * @config {string} [autoSortActiveKey] Local storage key for auto sort on
-         * @returns {bool}
-         */
-        getAutoOn: function () {
-            return jhelpers.jQueryStorageGetValue(config.autoSortActiveKey);
-        },
-
-        /**
-         * Set the old bookmark days in local storage
-         * @config {string} [oldBookmarkDaysKey] Local storage key for old bookmark days
-         * @param {int} value The int to set
-         */
-        setOldBookmarkDays: function (value) {
-            jhelpers.jQueryStorageSetValue(config.oldBookmarkDaysKey, value);
-        },
-
-        /**
-         * Get the old bookmark days in local storage
-         * @config {string} [autoSortPriorityKey] Local storage key for old bookmark days
-         * @config {string} [oldBookmarkDaysDefault] Default old bookmark days
-         * @returns {int}
-         */
-        getOldBookmarkDays: function () {
-            var oldBookmarkDays = jhelpers.jQueryStorageGetValue(config.oldBookmarkDaysKey);
-
-            return oldBookmarkDays === null ? config.oldBookmarkDaysDefault : oldBookmarkDays;
-        },
-
-        /**
-         * Set the sorting in progress  in local storage
-         * @config {string} [isSortingKey] Sorting in progress key
-         * @param {int} value The int to set
-         */
-        setIsSorting: function (value) {
-            jhelpers.jQueryStorageSetValue(config.isSortingKey, value);
-        },
-
-        /**
-         * Get the sorting in progress from storage
-         * @config {string} [isSortingKey] Sorting in progress key
-         * @returns {boolean}
-         */
-        getIsSorting: function () {
-            var me = this,
-                manualSorting = me.getIsOnManualSorting(),
-                createSorting = me.getIsOnCreateSorting(),
-                alarmSorting = me.getIsOnAlarmSorting(),
-                isSorting = manualSorting || createSorting || alarmSorting;
-
-            return isSorting;
-
-        },
-
-        /**
-         * Set the sorting in progress for the manual sorting in local storage
-         * @config {string} [isOnManualSortingKey] Sorting in progress key
-         * @param {int} value The int to set
-         */
-        setIsOnManualSorting: function (value) {
-            jhelpers.jQueryStorageSetValue(config.isOnManualSortingKey, value);
-        },
-
-        /**
-         * Get the sorting in progress from storage
-         * @config {string} [isOnManualSortingKey] Sorting in progress key
-         * @returns {boolean}
-         */
-        getIsOnManualSorting: function () {
-            return jhelpers.jQueryStorageGetValue(config.isOnManualSortingKey) || false;
-
-        },
-
-        /**
-         * Set the sorting in progress for the on created listener in local storage
-         * @config {string} [isOnCreateSortingKey] Sorting in progress key
-         * @param {int} value The int to set
-         */
-        setIsOnCreateSorting: function (value) {
-            jhelpers.jQueryStorageSetValue(config.isOnCreateSortingKey, value);
-        },
-
-        /**
-         * Get the sorting for the on created listener in progress from storage
-         * @config {string} [isOnCreateSortingKey] Sorting in progress key
-         * @returns {boolean}
-         */
-        getIsOnCreateSorting: function () {
-            return jhelpers.jQueryStorageGetValue(config.isOnCreateSortingKey) || false;
-
-        },
-
-        /**
-         * Set the sorting in progress  in local storage
-         * @config {string} [isOnIntervalSortingKey] Sorting in progress key
-         * @param {int} value The int to set
-         */
-        setIsOnAlarmSorting: function (value) {
-            jhelpers.jQueryStorageSetValue(config.isOnIntervalSortingKey, value);
-        },
-
-        /**
-         * Get the sorting in progress from storage
-         * @config {string} [isOnIntervalSortingKey] Sorting in progress key
-         * @returns {boolean}
-         */
-        getIsOnAlarmSorting: function () {
-            return jhelpers.jQueryStorageGetValue(config.isOnIntervalSortingKey) || false;
-
-        },
-
-        /**
-         * Set the maximum number of folders to create when using taxonomy
-         * @config {string} [maxTaxonomyFoldersKey]
-         * @param {int} value
-         */
-        setMaxTaxonomyLevels: function (value) {
-            jhelpers.jQueryStorageSetValue(config.maxTaxonomyFoldersKey, value);
-        },
-
-        /**
-         * Get the maximum number of folders to create when using taxonomy
-         * @config {string} [maxTaxonomyFoldersKey]
-         * @config {string} [defaultTaxonomyLevels]
-         * @returns {int}
-         */
-        getMaxTaxonomyLevels: function () {
-            var taxonomyLevels = jhelpers.jQueryStorageGetValue(config.maxTaxonomyFoldersKey);
-
-            return taxonomyLevels === null ? config.defaultTaxonomyLevels : taxonomyLevels;
-        },
-
-
-        setIsOnCullBookmarks: function (value) {
-            jhelpers.jQueryStorageSetValue(config.isOnCullBookmarks, value);
-        },
-
-
-        getIsOnCullBookmarks: function () {
-            return jhelpers.jQueryStorageGetValue(config.isOnCullBookmarks) || false;
-        },
-
-        setCullNumber: function (value) {
-            jhelpers.jQueryStorageSetValue(config.cullNumber, value);
-        },
-
-        getCullNumber: function () {
-            var cullLevels = jhelpers.jQueryStorageGetValue(config.cullNumber);
-
-            return cullLevels === null ? config.defaultCullNumber : cullLevels;
-        },
-
-        setSortAction: function (value) {
-            jhelpers.jQueryStorageSetValue(config.sortAction, value);
-        },
-
-        getSortAction: function () {
-            return jhelpers.jQueryStorageGetValue(config.sortAction);
-        },
-
-        getArchivesName: function() {
-            return jhelpers.jQueryStorageGetValue(config.archivesName) || config.archivesFolder;
-        },
-
-        setArchivesName: function(value) {
-            jhelpers.jQueryStorageSetValue(config.archivesName, value);
-        },
-
-        setAutosortCounter: function(value) {
-            jhelpers.jQueryStorageSetValue(config.indexCounter, value);
-        },
-
-        getAutosortCounter: function() {
-            return jhelpers.jQueryStorageGetValue(config.indexCounter);
-        }
-    };
+    return module;
 });
